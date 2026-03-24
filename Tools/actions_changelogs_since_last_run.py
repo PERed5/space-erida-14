765e1017c0f7c4cf625fdda5d8311e0dee73e178
Sidzaru <shinreki@proton.me>
2026-03-24 23:59:38 +0300

    Test 9

--- Tools/actions_changelogs_since_last_run.py
+++ Tools/actions_changelogs_since_last_run.py
@@ -51,10 +51,8 @@ def main():
         cur_changelog = yaml.safe_load(f)
 
     diff = diff_changelog(last_changelog, cur_changelog)
-
-    for entry in diff:
-        embed = entry_to_embed(entry)
-        send_embed(embed)
+    message_lines = changelog_entries_to_message_lines(diff)
+    send_message_lines(message_lines)
 
 
 def get_most_recent_workflow(
@@ -146,7 +144,7 @@ def get_discord_body(content: str):
         # Do not allow any mentions.
         "allowed_mentions": {"parse": []},
         # SUPPRESS_EMBEDS
-        "flags": 1 << 2,
+        # "flags": 1 << 2,
     }
 
 def send_with_retry(webhook_url: str, body: dict, name: str):
@@ -217,48 +215,6 @@ def changelog_entries_to_message_lines(entries: Iterable[ChangelogEntry]) -> lis
 
     return message_lines
 
-def entry_to_embed(entry: ChangelogEntry) -> dict:
-    lines = []
-
-    for change in entry["changes"]:
-        emoji = TYPES_TO_EMOJI.get(change["type"], "❓")
-        message = change["message"]
-
-        if len(message) > 4000:
-            message = message[:3900].rstrip() + "..."
-
-        lines.append(f"{emoji} - {message}")
-
-    description = "\n".join(lines)
-
-    embed = {
-        "title": entry["title"],
-        "description": description,
-        "color": 0xa06da8,
-        "footer": {
-            "text": entry.get("author"),
-            "icon_url": entry.get("avatar_url"),
-        },
-        "url": entry.get("url"),
-        "timestamp": entry["time"],
-    }
-
-    return embed
-
-def send_embed(embed: dict):
-    webhook_url = os.environ.get("DISCORD_WEBHOOK_URL_ERIDA")
-
-    if not webhook_url:
-        print("No Discord webhook configured!")
-        return
-
-    body = {
-        "embeds": [embed],
-        "allowed_mentions": {"parse": []},
-        "flags": 1 << 2,
-    }
-
-    send_with_retry(webhook_url, body, "Erida")
 
 def send_message_lines(message_lines: list[str]):
     """Join a list of message lines into chunks that are each below Discord's message length limit, and send them."""
