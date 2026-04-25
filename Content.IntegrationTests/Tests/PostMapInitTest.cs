3ba04ab9035eef2ccde3e7084ecade914805d360
Sidzaru <110207359+Sidzaru@users.noreply.github.com>
2026-04-25 08:09:30 +0000

    Revert "Train remap (#341)"
    This reverts commit e23b370e6f5b28285293d2b4b47b59220ac058f7.

--- Content.IntegrationTests/Tests/PostMapInitTest.cs
+++ Content.IntegrationTests/Tests/PostMapInitTest.cs
@@ -116,7 +116,6 @@ namespace Content.IntegrationTests.Tests
               "Loop_Erida",
               "Omega_Erida",
               "Packed_Erida",
-              "Train",
               "CentComm",      // CentComm
               "Dev",            // Dev map
               "Lavatest"       //Dev map
